e3dec9e9922d53565206766014cf7b161cbb1a06
kg-beep88 <kgchesterlee@gmail.com>
2025-12-26 14:35:15 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // config.js
 window.APP_CONFIG = {
   CLIENT_ID: "311576914016-t75jtnmu1k14o0mmarr0jhu49em9nb71.apps.googleusercontent.com",
-  CALENDAR_ID: "1072047281bc1c13bf7fcfe50b29e23cd26432d2af91daafe0448530c8973762@group.calendar.google.com",       // recommended: set to your "KG Demolish" calendar id
+  CALENDAR_ID: "primary",       // recommended: set to your "KG Demolish" calendar id
   DEFAULT_FILTER: "#KG",
   DEFAULT_DURATION_MIN: 120,
   TITLE_PREFIX: "KG "
