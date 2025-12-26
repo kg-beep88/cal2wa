1da4126f250b56d52140714740298fd36ed731d7
kg-beep88 <kgchesterlee@gmail.com>
2025-12-26 16:50:24 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // config.js
 window.APP_CONFIG = {
-  CLIENT_ID: "PASTE_YOUR_CLIENT_ID.apps.googleusercontent.com",
+  CLIENT_ID: "311576914016-t75jtnmu1k14o0mmarr0jhu49em9nb71.apps.googleusercontent.com",
   DEFAULT_FILTER: "#KG",
   DEFAULT_DURATION_MIN: 120,
   TITLE_PREFIX: "KG ",
