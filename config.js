6d601d5aaf178f5127b493f7342c5274e60d657b
kg-beep88 <kgchesterlee@gmail.com>
2026-01-08 17:08:23 +0800

    Update config.js

--- config.js
+++ config.js
@@ -3,5 +3,6 @@ window.APP_CONFIG = {
   CLIENT_ID: "311576914016-t75jtnmu1k14o0mmarr0jhu49em9nb71.apps.googleusercontent.com",
   DEFAULT_FILTER: "#KG",               // tag stored in DESCRIPTION for filtering
   DEFAULT_DURATION_MIN: 120,
-  CALENDAR_ID: "primary"               // default calendar; you can choose in app
+  CALENDAR_ID: "primary",              // calendar you schedule jobs into
+  BILLING_CALENDAR_ID: "f0b3394a57d1f0c0493153fa1eb17f5c71da82531eaa81122e95c403353a66f4@group.calendar.google.com"            // OPTIONAL: private calendar ID for billing copy (only you can see)
 };
